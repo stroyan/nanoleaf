a701d2fe3afc27da01ca636ab0b5fcf27e80d9c0
Mike Stroyan <mike@stroyan.net>
2019-03-13 12:17:21 -0600

    Added a panel info reading test.

---
+++ info.py
@@ -0,0 +1,7 @@
+from nanoleaf import Aurora
+
+backsplash = Aurora("nanoleaf", "gsFyJGvYGQzs818SsmBgn9eVyYe51WFg")
+#backsplash.on = True
+#backsplash.effect_random()
+info = backsplash.info
+print(info)
