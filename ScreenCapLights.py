3e5fc46d3ee4e8bf617a890fa396b0e7ef4457d1
Mike Stroyan <mike@stroyan.net>
2019-03-14 15:44:10 -0600

    Only update panel for screen parts that changed.

--- ScreenCapLights.py
+++ ScreenCapLights.py
@@ -14,21 +14,31 @@ for panel in info["panelLayout"]["layout"]["positionData"]:
     angle = math.radians(60)
     new_x = math.floor((x * math.cos(angle - y * math.sin(angle))) / 65 + 2.5)
     new_y = math.floor((x * math.sin(angle + y * math.cos(angle))) / 172 + 0.75)
-    panel_list.append({"panelId":str(panel["panelId"]),"x":new_x,"y":new_y})
+    unset_color=(-1,-1,-1)
+    panel_list.append({"panelId":str(panel["panelId"]),"x":new_x,"y":new_y,"color":unset_color})
 effect = {
     "command": "display",
     "animType": "static",
     "loop": False
 }
 for t in range(20):
+    start_time=time.time()
     im = PIL.ImageGrab.grab() 
     im.thumbnail([5,2])
     px = im.load()
-    effect["animData"] = str(len(panel_list)) + " "
+    panel_strings = []
+    effect["animData"] = ""
     for panel in panel_list:
-        r, g, b = px[panel["x"],panel["y"]]
-        effect["animData"] = effect["animData"] + "{} 1 {} {} {} 0 10 ".format(panel["panelId"], r, g, b)
+        color = px[panel["x"],panel["y"]]
+        if (color != panel["color"]):
+            panel["color"] = color
+            r, g, b = color
+            panel_strings.append("{} 1 {} {} {} 0 10 ".format(panel["panelId"], r, g, b))
 
-    print(effect)
-    backsplash.effect_set_raw(effect)
+    if(len(panel_strings) > 0):
+        effect["animData"] = str(len(panel_strings)) + " " + " ".join(panel_strings)
+        print(effect)
+        backsplash.effect_set_raw(effect)
+
+    print("sleep after {} seconds".format(time.time() - start_time))
     time.sleep(1)
